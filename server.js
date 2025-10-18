0932027ddb4bb5175cafbe604250e85fe9617e0d
Zang <22716371.giang@student.iuh.edu.vn>
2025-10-18 23:59:07 +0700

    git commit

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const db = require('./db');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const db = require("./db");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public"))); 
+app.use(express.static(path.join(__dirname, "public")));
 
 // Import các route
-const accountRoutes = require('./routes/accountRoutes');
-const bannerRoutes = require('./routes/bannerRoutes');
-const blogRoutes = require('./routes/blogRoutes');
-const customerRoutes = require('./routes/customerRoutes');
-const featureRoutes = require('./routes/featureRoutes');
-const instagramRoutes = require('./routes/instagramRoutes');
-const orderDetailRoutes = require('./routes/orderDetailRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const productRoutes = require('./routes/productRoutes');
-const userRoutes = require('./routes/userRoutes');
+const accountRoutes = require("./routes/accountRoutes");
+const bannerRoutes = require("./routes/bannerRoutes");
+const blogRoutes = require("./routes/blogRoutes");
+const customerRoutes = require("./routes/customerRoutes");
+const featureRoutes = require("./routes/featureRoutes");
+const instagramRoutes = require("./routes/instagramRoutes");
+const orderDetailRoutes = require("./routes/orderDetailRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 // Sử dụng các route cho phù hợp
-app.use('/api/accounts', accountRoutes);
-app.use('/api/banners', bannerRoutes);
-app.use('/api/blogs', blogRoutes);
-app.use('/api/customers', customerRoutes);
-app.use('/api/features', featureRoutes);
-app.use('/api/instagramPosts', instagramRoutes);
-app.use('/api/orderDetails', orderDetailRoutes);
-app.use('/api/orders', orderRoutes);  
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api/accounts", accountRoutes);
+app.use("/api/banners", bannerRoutes);
+app.use("/api/blogs", blogRoutes);
+app.use("/api/customers", customerRoutes);
+app.use("/api/features", featureRoutes);
+app.use("/api/instagramPosts", instagramRoutes);
+app.use("/api/orderDetails", orderDetailRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 // Khởi động server
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
