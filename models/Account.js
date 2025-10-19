f0e86982e685c419e8f7aa4a027255dcff45c89e
Zang <22716371.giang@student.iuh.edu.vn>
2025-10-19 16:05:44 +0700

    update_process_accountid

--- models/Account.js
+++ models/Account.js
@@ -1,15 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Định nghĩa schema cho Account
-const AccountSchema = new mongoose.Schema({
-  id: { type: String, required: true },
-  userName: { type: String, required: true },
-  password: { type: String, required: true },
-  email: { type: String, required: true },
-  role: { type: String, required: true }
-}, { versionKey: false });
+const AccountSchema = new mongoose.Schema(
+  {
+    id: { type: String, required: true },
+    userName: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    role: { type: String, required: true },
+  },
+  { versionKey: false }
+);
+
+AccountSchema.pre("save", async function (next) {
+  if (!this.id) {
+    const count = await mongoose.model("Account").countDocuments();
+    this.id = String(count + 1);
+  }
+  next();
+});
 
 // Tạo và xuất model từ schema
-const Account = mongoose.model('Account', AccountSchema);
+const Account = mongoose.model("Account", AccountSchema);
 
 module.exports = Account;
